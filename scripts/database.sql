55df3c6604297b42ebeba897d21bf6defb0369de
Punnapha24 <nunu4gang@hotmail.com>
2026-06-11 10:46:57 +0700

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,1 +1,28 @@
+-- Database: Data_Warehouse and Schemas: bronze, silver, gold
 
+use master;
+go
+
+-- Check if the database already exists and drop it if it does
+if exists (select * from sys.databases where name = 'Data_Warehouse')
+begin
+	alter database Data_Warehouse set single_user with rollback immediate; -- Force disconnect all users
+	drop database Data_Warehouse; -- Drop the existing database
+end;
+go
+
+-- Create the database
+create database Data_Warehouse;
+go
+
+use Data_Warehouse;
+go
+
+-- Create the schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
+ 
